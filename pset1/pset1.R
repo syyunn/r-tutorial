b51d4b3b4c02b6b40c7a633d490ec3cc9f06ba6b
Suyeol Yun <syyun@mit.edu>
2021-08-30 14:07:00 -0400

    add original

---
+++ pset1/pset1.R
@@ -0,0 +1,59 @@
+#### Math Camp 2020
+
+### Pset 1
+
+# Your name here:
+
+# What should you do before you start working on R??
+
+## Functions
+
+# Increasing the maximum. Let's go against our wishes and increase inequeality.
+# Create a function that will select the biggest value from a vector and add a number to it.
+# Set the default value for that number to be equal to 2
+
+# Try it with the following vector
+
+vector1<- c(1:11)
+
+
+############
+## Working with datasets
+
+# Install and open the packages that you will need
+
+
+
+# Get your data: load the dataset "antidumping.csv" using read.csv()
+
+
+# What kind of object is your dataset?
+
+
+# Get a sense of the data by checking the names of each of the variables,
+# looking at the dimensions, and
+
+
+# What are the dimensions of the data?
+
+
+# Take a look at the first lines
+
+
+# How many unique countries in this dataset
+
+
+# Make a subset of data for all observations for Mexico
+
+
+# What is the max number of antidumping cases that Mexico has recorded in this data?
+
+
+# In what year(s) did Mexico have this max value of antidumping cases? Select the rows
+# of this subset of data
+
+
+# What is the average gdp per capita that Mexico recorded during this period?
+
+
+# Order the mexican data by the value of their GDP per capita, but make it decreasing
