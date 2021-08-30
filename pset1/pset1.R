a8ba812d43844baddf59b05e6f51d2855d2333bf
Suyeol Yun <syyun@mit.edu>
2021-08-30 14:27:18 -0400

    add all

--- pset1/pset1.R
+++ pset1/pset1.R
@@ -2,33 +2,50 @@
 
 ### Pset 1
 
-# Your name here:
+# Your name here: Suyeol Yun
 
-# What should you do before you start working on R??
+# What should you do before you start working on R?
+# 1. Set working directory using setwd() or check it using getwd()
+# 2. Clear out previous memory variables using rm()
 
 ## Functions
 
 # Increasing the maximum. Let's go against our wishes and increase inequeality.
 # Create a function that will select the biggest value from a vector and add a number to it.
+
+imax <- function(v, n) {
+  return(tail(sort(v), 1) + n)
+}
+
+## unit-testing
+imax(c(1,2,3,4), 1)
+
 # Set the default value for that number to be equal to 2
+imax_default <- function(v, n=2) {
+  return(tail(sort(v), 1) + n)
+}
+
+## unit-testing
+imax_default(c(1,2,3,4))
+
 
 # Try it with the following vector
-
 vector1<- c(1:11)
+imax(vector1, n=1)
+imax_default(vector1)
 
 
 ############
 ## Working with datasets
 
 # Install and open the packages that you will need
-
-
+## install.packages("utils") -- base package
 
 # Get your data: load the dataset "antidumping.csv" using read.csv()
-
+ad <- read.csv('./antidumping.csv')
 
 # What kind of object is your dataset?
-
+class(ad) # which prints class
 
 # Get a sense of the data by checking the names of each of the variables,
 # looking at the dimensions, and
